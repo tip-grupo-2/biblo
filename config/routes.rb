979614d52eae7e154fa36d649b3196cf120bf746
tini <agustinariq@gmail.com>
2018-09-02 20:28:44 -0300

    Interfaz y mensajes en espanol

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root "biblioteca#show", page: "home"
 
   # devise
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
 end
