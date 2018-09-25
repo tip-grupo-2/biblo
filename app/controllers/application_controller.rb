aaca32a3622dc2c94dbb4c42f08c0d25a9df3406
tini <agustinariq@gmail.com>
2018-09-24 23:29:10 -0300

    Actualizo estilo y muestro notificaciones en dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def notifications(user)
+    Notification.where(recipient_id: user.id)
+  end
+  helper_method :notifications
+
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,7 @@
 class Notification < ActiveRecord::Base
   belongs_to :recipient, class_name: User
   belongs_to :requester, class_name: User
+  belongs_to :copy, class_name: Book
 
   def self.new_request_notification(requester, copy)
     Notification.new(requester_id: requester.id, recipient_id: copy.user_id, copy_id: copy.id, action: 'solicitado')
