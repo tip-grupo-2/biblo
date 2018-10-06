4fedfdb477bb08472924b5ba6f9dccecb8d4978b
tini <agustinariq@gmail.com>
2018-10-06 16:12:06 -0300

    Usuario puede editar su perfil (cambiart avatar y datos)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
- def show
-  @users = User.all
-  @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-   marker.lat user.latitude
-   marker.lng user.longitude
+  def show
+    @users = User.all
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+    marker.lat user.latitude
+    marker.lng user.longitude
+    end
   end
- end
+
+   def edit
+     @user = User.find(params[:id])
+   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :address, :avatar)
+  end
 end
