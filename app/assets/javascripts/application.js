ff87606f7008e31d6dd1ca42b80fc27386b20a58
carlos <lalopsb@hotmail.com>
2018-10-13 18:27:09 -0300

    Added menu on navbar - details and picture on books

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,12 @@
 //= require_tree .
 //= require underscore
 //= require gmaps/google
+//= require bootstrap
+
+$(document).ready(function () {
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+        return this.href == url;
+    }).parent().addClass('active');
+});
