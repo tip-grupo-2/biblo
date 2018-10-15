978d9582769dfb4e24acc095bddaf5236e1403c7
lauti910 <lauti910@gmail.com>
2018-10-14 22:14:05 -0300

    Fix indexes copies column
     - Se agrega la columna copias a todos los index
     - En la de donaciones y mis libros, aparece la cantidad de copias
     - En libros para pedir, aparecen las diferentes copias

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,9 @@
 class Book < ActiveRecord::Base
   validates :isbn, uniqueness: true, length: { is: 13 }, presence: true
   has_many :copies
-  has_one :user # TODO: agregar tabla intermedia para dueño actual
+  has_one :user #dueño acutal
 
   ISBN_PROVIDER_ERROR = Class.new(StandardError)
   ISBN_LENGTH_ERROR = Class.new(StandardError)
+
 end
