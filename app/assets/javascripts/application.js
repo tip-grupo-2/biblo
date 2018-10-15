af58067782866377e425e883699830c9ff370f98
carlos <lalopsb@hotmail.com>
2018-10-14 22:41:44 -0300

    Agrego redireccion a edit user si no hay domicilio en el sign up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,3 @@
 //= require gmaps/google
 //= require bootstrap
 
-$(document).ready(function () {
-    var url = window.location;
-    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
-    $('ul.nav a').filter(function() {
-        return this.href == url;
-    }).parent().addClass('active');
-});
