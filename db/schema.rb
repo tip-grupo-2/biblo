e743c47ca37d44932ee6978e9195e8ac80474d42
tini <agustinariq@gmail.com>
2018-10-18 18:58:13 -0300

    Integracion con whatsapp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181018013443) do
+ActiveRecord::Schema.define(version: 20181018205636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20181018013443) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "avatar"
+    t.string   "phone_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
