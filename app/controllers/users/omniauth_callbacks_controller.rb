3a1972d7f3014bc8a6c905485819f58e0813bef6
tini <agustinariq@gmail.com>
2018-10-21 22:23:48 -0300

    Notificaciones fade out

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def authenticate_user
-    puts session['devise.user_attributes']
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user.persisted?
       flash[:info] = 'Bienvenido/a!'
