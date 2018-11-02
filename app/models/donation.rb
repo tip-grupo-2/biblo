db897f7574afb29a4bea6098b4fb26151164322f
lauti910 <lauti910@gmail.com>
2018-11-02 00:22:04 -0300

    Comentarios
     - Como utilizar los states de donations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -4,6 +4,7 @@ class Donation < ActiveRecord::Base
   belongs_to :requester, class_name: User    #El que pidio la copia
   belongs_to :copy                           #La copia donada
   aasm column: 'state' do
+    #Los estados posibles de la donacion
     state :donated, initial: true
     state :requested
     state :accepted
@@ -11,6 +12,24 @@ class Donation < ActiveRecord::Base
     state :receive_confirmed
     state :finished
 
+    #Como cambiar de un estado a otro:
+    # donacion = Donation.new
+    # donacion.state # returns donated
+    #
+    # donacion.request(requester_user)
+    # donacion.state # returns requested
+    #
+    # donacion.accept
+    # donacion.state # returns accepted
+    # donacion.confirm_delivery || donacion.confirm_receive
+    # donacion.state # returns delivery_confirmed || receive_confirmed
+    # donacion.confirm_delivery || donacion.confirm_receive (el que falte de los 2)
+    # donacion.state # returns finished
+    #
+    # Osea:
+    #                                    -> receive_confirmed -> deliver_confirmed v
+    # donated -> requested -> accepted -x                                           x> finished
+    #     ^--------------reject          -> deliver_confirmed -> receive_confirmed ^
     event :request, after: :set_requester do
       transitions from: [:donated], to: :requested
     end
@@ -18,6 +37,9 @@ class Donation < ActiveRecord::Base
       transitions from: [:requested], to: :accepted
     end
     event :reject do
+      before do
+        self.requester = nil
+      end
       transitions from: [:requested, :accepted], to: :donated
     end
     event :confirm_delivery do
