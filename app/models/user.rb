ba0f9a71fe1bd494e2ffc84b31dd3e4f8e07207c
lauti910 <lauti910@gmail.com>
2018-11-01 22:41:46 -0300

    Fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ActiveRecord::Base
   has_many :copies # Donaciones
   has_many :books  # Libros que lee
   has_many :notifications, foreign_key: :recipient_id
-  has_many :donations, inverse_of: :giver
   devise :database_authenticatable, :registerable, :rememberable, :validatable, :timeoutable, :omniauthable,
          omniauth_providers: %i[facebook google_oauth2]
 
--- db/migrate/20181102011723_add_donation_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddDonationToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :donation_id, :integer
-  end
-end
