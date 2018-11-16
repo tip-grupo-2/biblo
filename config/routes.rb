1fb98bedffa3359737353efaeeb1c062ea7d7bf3
tini <agustinariq@gmail.com>
2018-11-16 00:58:38 -0300

    Donacion de libros: agrego redirects al refrescar rutas POST. Centro y alineo botones. Rescue de excepcion. Acentos.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   get '/my_books', to: 'books#index_my_books', as: 'my_books'
   root 'biblioteca#show', page: 'home'
   post '/preview_book', to: 'books#preview'
+  get '/preview_book', to: redirect('books/new')
   post '/preview_book_title', to: 'books#preview_title'
+  get '/preview_book_title', to: redirect('books/new')
   post '/create_manual_book', to: 'books#create_manual'
+  get '/manual_book', to: redirect('books/new')
   post '/manual_book', to: 'books#enter_manual'
   put '/finish_book/:id(.:format)', to: 'books#finish', as: 'finish_book'
   put '/start_book/:id(.:format)', to: 'books#start', as: 'start_book'
