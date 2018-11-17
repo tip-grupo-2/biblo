5acd4ad3f96baf4f1f04758d8258aa7da6a2bc42
carlos <lalopsb@hotmail.com>
2018-11-17 15:41:04 -0300

    Mejoras en home

---
+++ app/helpers/view_helper.rb
@@ -0,0 +1,29 @@
+module ViewHelper
+  SLIDE_ONE = {
+      main_caption: 'Cataloga todos tus libros!',
+      sub_caption: 'Nunca vuelvas a perder un libro, o a olvidar que lo prestaste',
+      back_image: 'biblo_01_1800_500.jpg'
+  }
+  SLIDE_TWO = {
+      main_caption: 'Qué querés leer hoy?',
+      sub_caption: 'Tu próxima aventura puede estar a metros de distancia!',
+      back_image: 'biblo_02_1800_500.jpg'
+  }
+  SLIDE_THREE = {
+      main_caption: 'Libros guardados en un placard?',
+      sub_caption: 'Tal vez alguien muy cerca quiere leerlos!',
+      back_image: 'biblo_03_1800_500.jpg'
+  }
+  SLIDE_FOUR = {
+      main_caption: 'Empezando un nuevo año escolar?',
+      sub_caption: 'Seguro te sirven los libros de alguien que cursó el año anterior!',
+      back_image: 'biblo_04_1800_500.jpg'
+  }
+
+  SLIDE_DATA = [
+      SLIDE_ONE,
+      SLIDE_TWO,
+      SLIDE_THREE,
+      SLIDE_FOUR
+  ]
+end
