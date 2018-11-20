2c8a55776882959f8c8d0335f9f2cf248cf0d76b
lauti910 <lauti910@gmail.com>
2018-11-20 04:24:55 -0300

    Se me rompio git y tube que recrear la branch puntuacion

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,13 @@ class Book < ActiveRecord::Base
   ISBN_PROVIDER_ERROR = Class.new(StandardError)
   ISBN_LENGTH_ERROR = Class.new(StandardError)
 
+  def average_rate
+    rates = Rate.where(:book_id => :id).pluck(:amount)
+    average = 0
+    if rates.count > 0
+      average = rates.sum / rates.count
+    end
+
+    average
+  end
 end
