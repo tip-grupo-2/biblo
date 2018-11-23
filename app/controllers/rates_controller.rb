eb42bc98acb39c0cc828fb93f1cd6cacd8e36392
lauti910 <lauti910@gmail.com>
2018-11-23 01:28:53 -0300

    Agrego las calificaciones del estado del libro

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -2,8 +2,9 @@ class RatesController < ApplicationController
 
 
   def book_rating
-    @book = Book.find(params[:id])
-    @ratings = Rate.where(book_id: params[:id]).all
+    @donation = Donation.find(params[:id])
+    @book_ratings = Rate.where(book_id: @donation.copy.book.id).all
+    @copy_ratings = Rate.where(copy_id: @donation.copy.id).all
 
   end
 end
